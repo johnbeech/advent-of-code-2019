8a1e6f4e80759ded68b911378263134c6a68c891
John Beech <john.beech@bbc.co.uk>
2019-12-04 15:07:29 +0000

    Solved day4 for first star

--- solutions/day4/solution.js
+++ solutions/day4/solution.js
@@ -10,8 +10,43 @@ async function run () {
   await solveForSecondStar(input)
 }
 
+function testValidPassword (passwordValue) {
+  let passwordContainsDouble = false
+  let passwordDigitsNeverDecrease = true
+
+  const sequence = (passwordValue + '').split('').map(n => Number.parseInt(n))
+  let previousNumber
+  sequence.forEach(number => {
+    if (previousNumber !== undefined) {
+      if (previousNumber === number) {
+        passwordContainsDouble = true
+      }
+      if (previousNumber > number) {
+        passwordDigitsNeverDecrease = false
+      }
+    }
+    previousNumber = number
+  })
+
+  return passwordContainsDouble && passwordDigitsNeverDecrease
+}
+
 async function solveForFirstStar (input) {
-  const solution = 'UNSOLVED'
+  const [, startInput, endInput] = input.match(/(\d{6})-(\d{6})/)
+  report('Start Input', startInput, 'End Input', endInput)
+
+  let password = startInput
+  const validPaswords = []
+  while (password <= endInput) {
+    if (testValidPassword(password)) {
+      validPaswords.push(password)
+    }
+    // do something
+    password++
+  }
+
+  const solution = validPaswords.length
+  report('Valid Passwords', validPaswords)
   report('Input:', input)
   report('Solution 1:', solution)
 }
