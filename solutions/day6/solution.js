6746403ed797146654bd3cc46798f365145ffef6
Markavian <github@mkv25.net>
2019-12-06 07:59:00 +0000

    Solve day 6 for first star

--- solutions/day6/solution.js
+++ solutions/day6/solution.js
@@ -11,8 +11,28 @@ async function run () {
 }
 
 async function solveForFirstStar (input) {
-  const solution = 'UNSOLVED'
-  report('Input:', input)
+  const separator = ')'
+  const orbits = input.split('\n').map(n => n.trim().split(separator))
+  const orbitMap = orbits.reduce((acc, orbit) => {
+    const [a, b] = orbit
+    acc[a] = acc[a] || {}
+    acc[b] = acc[b] || {}
+    acc[a][b] = acc[b]
+    acc[b].parent = acc[a]
+    return acc
+  }, {})
+
+  const orbitCount = Object.entries(orbitMap).reduce((acc, orbit) => {
+    const [, value] = orbit
+    let i = 0; let parent = value.parent
+    while (parent) {
+      i++
+      parent = parent.parent
+    }
+    return acc + i
+  }, 0)
+
+  const solution = orbitCount
   report('Solution 1:', solution)
 }
 
