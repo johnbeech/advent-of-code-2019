e1daba972934c9f5806d7f1afc9f266bfa746815
Markavian <github@mkv25.net>
2019-12-07 09:51:53 +0000

    Computed an incorrect answer

--- solutions/day7/solution.js
+++ solutions/day7/solution.js
@@ -5,24 +5,43 @@ const report = (...messages) => console.log(`[${require(fromHere('../../package.
 const compute = require('./intcode')
 
 async function run () {
-  const input = (await read(fromHere('example.txt'), 'utf8')).trim()
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
 
   await solveForFirstStar(input)
   await solveForSecondStar(input)
 }
 
 async function solveForFirstStar (instructions) {
-  const phaseSettings = '1,0,4,3,2'.split(',').map(n => Number.parseInt(n))
-
-  let signal = 0
-  while (phaseSettings.length) {
-    const phaseSetting = phaseSettings.shift()
-    const amplification = compute(instructions, [phaseSetting, signal])
-    report('Phase setting', phaseSetting, 'amplifies', signal, 'to', amplification.outputs[0])
-    signal = amplification.outputs[0]
+  const totalCombinations = Math.pow(5, 5)
+  const phases = []
+  while (phases.length < totalCombinations) {
+    const phaseSettings = phases.length.toString(5).split('').map(n => Number.parseInt(n))
+    while (phaseSettings.length < 5) {
+      phaseSettings.unshift(0)
+    }
+    phases.push(phaseSettings)
   }
 
-  const solution = signal
+  report('Phase combination count', phases.length)
+
+  let maxSignal = 0
+  while (phases.length > 0) {
+    let signal = 0
+    const phaseSettings = phases.pop()
+    const currentSettings = phaseSettings.join(', ')
+    while (phaseSettings.length) {
+      const phaseSetting = phaseSettings.shift()
+      const amplification = compute(instructions, [phaseSetting, signal])
+      // report('Phase setting', phaseSetting, 'amplifies', signal, 'to', amplification.outputs[0])
+      signal = amplification.outputs[0]
+    }
+    if (signal > maxSignal) {
+      report(currentSettings, 'New max found', signal, '>', maxSignal)
+    }
+    maxSignal = Math.max(maxSignal, signal)
+  }
+
+  const solution = maxSignal
   report('Solution 1:', solution)
 }
 
