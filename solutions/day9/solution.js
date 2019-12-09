49b3059c35a650d7a0e939c9c5c08b7383451462
John Beech <johnbeech.github@mkv25.net>
2019-12-09 23:50:55 +0000

    Measure execution time for parts 1 and 2

--- solutions/day9/solution.js
+++ solutions/day9/solution.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { read, position } = require('promise-path')
 const fromHere = position(__dirname)
 const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+const { performance } = require('perf_hooks')
 
 const compute = require('./intcode')
 
@@ -24,15 +25,25 @@ async function solveForTests () {
 }
 
 async function solveForFirstStar (input) {
+  const t0 = performance.now()
+
   const boost = await compute(input, [1])
   const solution = boost.outputs
   report('Solution 1:', solution)
+
+  const t1 = performance.now()
+  report('Solve for first star took:', (t1 - t0), 'milliseconds')
 }
 
 async function solveForSecondStar (input) {
+  const t0 = performance.now()
+
   const boost = await compute(input, [2])
   const solution = boost.outputs
   report('Solution 2:', solution)
+
+  const t1 = performance.now()
+  report('Solve for second star took:', (t1 - t0), 'milliseconds')
 }
 
 run()
