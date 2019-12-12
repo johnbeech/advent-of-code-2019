b7bc33c4cf72b672d93e1528ee3058f5c9600f39
Markavian <github@mkv25.net>
2019-12-12 22:34:16 +0000

    Solve day 12 for second star; with some help from google

--- solutions/day12/solution.js
+++ solutions/day12/solution.js
@@ -74,14 +74,69 @@ async function solveForFirstStar (input, stepCount = 1000) {
 
   const solution = sum(lastStep.map(n => n.total))
 
-  report('Input:', orbits)
-  report('Output:', lastStep)
   report('Solution 1:', solution)
 }
 
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const orbits = parseOrbits(input)
+
+  let universe = orbits
+  let xCycle = 0
+  do {
+    universe = stepOrbits(universe)
+    xCycle++
+  } while (
+    orbits[0].x !== universe[0].x ||
+    orbits[1].x !== universe[1].x ||
+    orbits[2].x !== universe[2].x ||
+    orbits[3].x !== universe[3].x ||
+    orbits[0].dx !== universe[0].dx ||
+    orbits[1].dx !== universe[1].dx ||
+    orbits[2].dx !== universe[2].dx ||
+    orbits[3].dx !== universe[3].dx
+  )
+  report('X repeats after ', xCycle, 'cycles')
+
+  universe = orbits
+  let yCycle = 0
+  do {
+    universe = stepOrbits(universe)
+    yCycle++
+  } while (
+    orbits[0].y !== universe[0].y ||
+    orbits[1].y !== universe[1].y ||
+    orbits[2].y !== universe[2].y ||
+    orbits[3].y !== universe[3].y ||
+    orbits[0].dy !== universe[0].dy ||
+    orbits[1].dy !== universe[1].dy ||
+    orbits[2].dy !== universe[2].dy ||
+    orbits[3].dy !== universe[3].dy
+  )
+  report('Y repeats after ', yCycle, 'cycles')
+
+  universe = orbits
+  let zCycle = 0
+  do {
+    universe = stepOrbits(universe)
+    zCycle++
+  } while (
+    orbits[0].z !== universe[0].z ||
+    orbits[1].z !== universe[1].z ||
+    orbits[2].z !== universe[2].z ||
+    orbits[3].z !== universe[3].z ||
+    orbits[0].dz !== universe[0].dz ||
+    orbits[1].dz !== universe[1].dz ||
+    orbits[2].dz !== universe[2].dz ||
+    orbits[3].dz !== universe[3].dz
+  )
+  report('Z repeats after', zCycle, 'cycles')
+
+  const solution = [xCycle, yCycle, zCycle].reduce(lcm)
   report('Solution 2:', solution)
 }
+
+/* Found on: https://stackoverflow.com/questions/47047682/least-common-multiple-of-an-array-values-using-euclidean-algorithm */
+const gcd = (a, b) => a ? gcd(b % a, a) : b
+const lcm = (a, b) => a * b / gcd(a, b)
 
 run()
