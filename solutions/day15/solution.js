0fd347641add0d270e027ef2ae85be21d6d936a9
Markavian <github@mkv25.net>
2019-12-15 09:55:00 +0000

    Create day 15 from template; with intcode comptuer code

---
+++ solutions/day15/solution.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const { read, position } = require('promise-path')
+const fromHere = position(__dirname)
+const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+
+const compute = require('./intcode')
+
+async function run () {
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+
+  await solveForFirstStar(input)
+  await solveForSecondStar(input)
+}
+
+async function solveForFirstStar (input) {
+  const solution = 'UNSOLVED'
+  report('Input:', input)
+  report('Solution 1:', solution)
+}
+
+async function solveForSecondStar (input) {
+  const solution = 'UNSOLVED'
+  report('Solution 2:', solution)
+}
+
+run()
